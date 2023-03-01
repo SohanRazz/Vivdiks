888b7eb2f8f99285850886118247d9c80e5b0459
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-03-01 12:02:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/SohanRazz/bulk.git /bulk
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /URL-Shortener-V2
+  git clone $UPSTREAM_REPO /bulk
 fi
-cd /URL-Shortener-V2
+cd /bulk
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
