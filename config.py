3fe12f007cb990ecc398ae49fd8f164f375faeea
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-03-01 17:09:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ ADMINS.append(5897793065) if OWNER_ID not in ADMINS else []
 LOG_CHANNEL = int(
     os.environ.get("LOG_CHANNEL", "1001544056917")
 )  # log channel for information about users
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL","-1001713432930")  # For Force Subscription
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL","-1001836945914")  # For Force Subscription
 BROADCAST_AS_COPY = is_enabled(
     (os.environ.get("BROADCAST_AS_COPY", "True")), True
 )  # true if forward should be avoided
