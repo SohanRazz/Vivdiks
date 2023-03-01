7b13e3bbf143500a121904a023d59563c5ff6a50
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-03-01 17:04:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 # Mandatory variables for the bot to start
 API_ID = int(os.environ.get("API_ID","29219170"))  # API ID from https://my.telegram.org/auth
 API_HASH = os.environ.get("API_HASH","12d6648ede66e1ef31d9c455317ee09d")  # API Hash from https://my.telegram.org/auth
-BOT_TOKEN = os.environ.get("BOT_TOKEN","5819018104:AAEMlmI8FH7ZO85FthGAMZ7GX1EsPUBVE5I")  # Bot token from @BotFather
+BOT_TOKEN = os.environ.get("BOT_TOKEN","5630279176:AAEGCGfyOaCMWcEeklIAVgADgQTwM0QHD8Q")  # Bot token from @BotFather
 ADMINS = ([int(i.strip()) for i in os.environ.get("ADMINS","5897793065").split(",")]
     if os.environ.get("ADMINS","5897793065")
     else [])
