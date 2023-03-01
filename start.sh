5b7ce774a0d0421e4e4a94d17710a4e6b004207d
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-03-01 11:56:57 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /URL-Shortener-V2
+fi
+cd /URL-Shortener-V2
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
